204f5468bf9ee7c1a5ac2998c91177f7f03d63ee
hiroyaiyori <vp054az116@gmail.com>
2022-10-28 22:04:53 +0900

    ADD skin nft

---
+++ src/sbt/ISbt.sol
@@ -0,0 +1,20 @@
+interface ISbt {
+    /**
+     * @dev Emitted when `owner` enables `approved` to manage the `tokenId` token.
+     */
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+
+    /**
+     * @dev Emitted when `owner` enables or disables (`approved`) `operator` to manage all of its assets.
+     */
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+    /**
+     * @dev Returns the number of tokens in ``owner``'s account.
+     */
+    function balanceOf(address owner) external view returns (uint256 balance);
+
+    /**
+     * @dev Returns the owner of the `tokenId` token.
+     *
+     * Requirements:
