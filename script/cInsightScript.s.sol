ad5d0fff88cb50b3d67c68f86a9a1d85f2e85a77
hidenoriwakiri <rtuocck2.020.hi@gmail.com>
2022-11-02 21:48:39 +0900

    MOD: script sol

--- script/cInsightScript.s.sol
+++ script/cInsightScript.s.sol
@@ -8,11 +8,22 @@ import {SkinNft} from "src/skinnft/SkinNft.sol";
 import {ISkinNft} from "src/skinnft/ISkinNft.sol";
 
 contract cInsightScript is Script {
+    ChainInsightGovernanceProxyV1 proxy;
+    ChainInsightLogicV1 logic;
+    ChainInsightExecutorV1 executor;
     Sbt sbt;
     SbtImp sbtImp;
     SkinNft skinNft;
 
-    address admin = address(9);
+    address admin = address(1);
+    address vetoer = address(2);
+
+    uint256 executingGracePeriod = 11520;
+    uint256 executingDelay = 11520;
+    uint256 votingPeriod = 5760;
+    uint256 votingDelay = 1;
+    uint8 proposalThreshold = 1;
+
     string baseURL = "https://thechaininsight.github.io/";
 
     function run() public {
@@ -21,12 +32,40 @@ contract cInsightScript is Script {
         // excute operations as a deployer account until stop broadcast
         vm.startBroadcast(deployerPrivateKey);
 
+        logic = new ChainInsightLogicV1();
+        newLogic = new ChainInsightLogicV1();
+        executor = new ChainInsightExecutorV1(address(logic));
         sbt = new Sbt();
         sbtImp = new SbtImp();
         skinNft = new SkinNft(string.concat(baseURL, "skinnft/"));
 
-        sbt.init(
+        vm.prank(proxy);
+        proxy = new ChainInsightGovernanceProxyV1(
+            address(logic),
+            address(executor),
+            address(sbt),
             admin,
+            vetoer,
+            executingGracePeriod,
+            executingDelay,
+            votingPeriod,
+            votingDelay,
+            proposalThreshold
+        );
+
+        logic.initialize(
+            address(executor),
+            address(sbt),
+            vetoer,
+            executingGracePeriod,
+            executingDelay,
+            votingPeriod,
+            votingDelay,
+            proposalThreshold
+        );
+
+        sbt.init(
+            address(executor),
             "ChainInsight",
             "SBT",
             string.concat(baseURL, "sbt/"),
